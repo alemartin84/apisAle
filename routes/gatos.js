aca79fc427f6ed3ce050fa7b09ef0efc7329c40c
alemartin84 <56177717+alemartin84@users.noreply.github.com>
2022-10-19 00:21:33 -0300

    Update gatos.js
    saco facts

--- routes/gatos.js
+++ routes/gatos.js
@@ -3,11 +3,10 @@ var router = express.Router();
 const gatosModels = require("./../models/gatosModels");
 
 router.get("/", async function (req, res, next) {
-  let respuesta = await gatosModels.getHecho();
+  
   let fotoGatito = await gatosModels.getCatPic();
 
   res.render("gatos", {
-    hechos: respuesta,
     ruta: fotoGatito,
     title: "Gatitos 🐱‍👤",
   });
